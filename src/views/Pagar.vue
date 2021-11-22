89a5e12cdf77ed75435cf24b0a77fab659cd03cc
acostalinan <brunosanto123@hotmail.com>
2021-11-21 23:38:22 -0500

    Add Pagar View
    Add Extra View

---
+++ src/views/Pagar.vue
@@ -0,0 +1,23 @@
+<template>
+  <Header/>
+     "actionButtons": {
+		 "reloadButton": null,
+		 "loadNpmCommands": false,
+		 "commands": [
+			 {
+				 "name": "Pagar",
+				 "singleInstance": true,
+				 "color": "#af565c",
+				 "command": "cargo run ${file}",
+			 },
+		 ]
+	 }
+</template>
+
+<script>
+import Header from "../components/header";
+export default {
+  name: "Anuncios",
+  components: {Header}
+}
+</script>
